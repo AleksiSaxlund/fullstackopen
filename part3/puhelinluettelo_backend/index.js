90b092ddd0d7220720c918258deb7830ba551b03
Aleksi Saxlund <saxlunda@ad.helsinki.fi>
2026-05-21 13:17:57 +0300

    Added dist

--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -5,8 +5,9 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
+
 
 let persons = [
   {
