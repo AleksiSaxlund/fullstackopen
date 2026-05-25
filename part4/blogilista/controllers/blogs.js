044c1048a5361f299ebe4881964625f2044cecb2
Aleksi Saxlund <saxlunda@ad.helsinki.fi>
2026-05-25 19:03:20 +0300

    Exercises 4.5, 4.8 - 4.14

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,18 +1,41 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
   })
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  //console.log(blog)
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes = 0 } = request.body
+
+  const blog = await Blog.findById(request.params.id)
+  
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  blog.title = title
+  blog.author = author
+  blog.url = url
+  blog.likes = likes
+
+  const updatedBlog = await blog.save()
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
---
+++ part4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,151 @@
+const assert = require('node:assert')
+const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('returned blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('proper amount is returned', async () => {
+    const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+
+  test('have proper id field', async () => {
+    const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual('id' in response.body[0], true)
+    assert.strictEqual('_id' in response.body[0], false)
+  })
+})
+
+describe('adding blogs', () => {
+  test('increases saved blog count', async () => {
+    await api
+      .post('/api/blogs')
+      .send(helper.newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  })
+  
+  test('adding blog with no value on likes', async () => {
+    const response = await api
+      .post('/api/blogs')
+      .send(helper.blogWithNoLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    //const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.likes, 0)
+  })
+
+  test('adding blog with no title returns error', async () => {
+    const response = await api
+      .post('/api/blogs')
+      .send(helper.blogWithNoTitle)
+      .expect(400)
+    
+    assert.strictEqual(response.status, 400)
+  })
+
+  test('adding blog with no url returns error', async () => {
+    const response = await api
+      .post('/api/blogs')
+      .send(helper.blogWithNoUrl)
+      .expect(400)
+    
+    assert.strictEqual(response.status, 400)
+  })
+})
+
+describe('deleting blogs', () => {
+  test('deletes blog from database', async () => {
+    const notesAtStart = await helper.blogsInDb()
+    const blogToDelete = notesAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+    
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const ids = blogsAtEnd.map(b => b.id)
+    assert(!ids.includes(blogToDelete.id))
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
+})
+
+describe('updating blogs', () => {
+  test('updated blog in database', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const updatedBlog = {
+      ...blogToUpdate,
+      likes: blogToUpdate.likes + 1
+    }
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const blogToCheck = blogsAtEnd.find(b => b.id === blogToUpdate.id)
+
+    assert.strictEqual(blogToCheck.likes, blogToUpdate.likes + 1)
+  })
+
+  test.only('updated blog with no likes field works', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const { likes, ...blogWithNoLikes } = blogToUpdate
+
+    await api
+      .put(`/api/blogs/${blogWithNoLikes.id}`)
+      .send(blogWithNoLikes)
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const blogToCheck = blogsAtEnd.find(b => b.id === blogWithNoLikes.id)
+
+    assert.strictEqual(blogToCheck.likes, 0)
+  })
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
